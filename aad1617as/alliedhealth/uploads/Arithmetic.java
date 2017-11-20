00ea691a7769636353f04236b137a468eb86173f
sabbakilam <pitdrop@gmail.com>
2017-11-20 06:35:52 +0000

    disable backbutton for DocumentLoading.html

---
+++ aad1617as/alliedhealth/uploads/Arithmetic.java
@@ -0,0 +1,10 @@
+public class Arithmetic{
+     public static void main(String [] args){
+         /**instantiate a DoMath object
+          * passing it the command line arguments
+         */
+          
+         new DoMath(args);
+       
+     }
+}
